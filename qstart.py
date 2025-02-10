8fb1210f25f3b74fd32973730f42eef4ad6e04b1
electric-otter <electric-otter-github@proton.me>
2025-02-10 09:17:07 -0500

    Update qstart.py

--- qstart.py
+++ qstart.py
@@ -32,6 +32,20 @@ from .utils import (
     replace_added_token,
     seed_everything,
 )
+from torchscale.architecture.decoder import Decoder, DecoderLayer
+from torchscale.architecture.encoder import Encoder, EncoderLayer
+from torchscale.component.dilated_attention import DilatedAttention
+from fairscale.nn import checkpoint_wrapper, wrap
+import torch
+import torch.nn as nn
+
+from torchscale.architecture.encoder import Encoder
+from torchscale.component.embedding import (
+    PositionalEmbedding,
+    TextEmbedding,
+    VisionEmbedding,
+)
+from torchscale.component.multiway_network import MutliwayEmbedding
 
 model = BertForMaskedLM(config=config)
 training_args = TrainingArguments(
@@ -74,7 +88,7 @@ class TransformerEncoderLayer(tf.keras.layers.Layer):
         out2 = self.layernorm2(out1 + ffn_output)
 
         return out2
-      class TransformerDecoderLayer(tf.keras.layers.Layer):
+class TransformerDecoderLayer(tf.keras.layers.Layer):
     def __init__(self, d_model, num_heads, dff, rate=0.1):
         super(TransformerDecoderLayer, self).__init__()
         self.mha1 = MultiHeadAttention(num_heads, d_model)
@@ -107,7 +121,7 @@ class TransformerEncoderLayer(tf.keras.layers.Layer):
         out3 = self.layernorm3(ffn_output + out2)
 
         return out3, attn_weights_block1, attn_weights_block2
-      class Transformer(tf.keras.Model):
+class Transformer(tf.keras.Model):
     def __init__(self, num_layers, d_model, num_heads, dff, input_vocab_size, 
                  target_vocab_size, pe_input, pe_target, rate=0.1):
         super(Transformer, self).__init__()
@@ -125,34 +139,152 @@ class TransformerEncoderLayer(tf.keras.layers.Layer):
             tar, enc_output, training, look_ahead_mask, dec_padding_mask)
 
         final_output = self.final_layer(dec_output)
+# Copyright (c) 2022 Microsoft
+# Licensed under The MIT License [see LICENSE for details] (https://github.com/microsoft/torchscale/blob/main/torchscale/model/BEiT3.py)
+
+
+
+
+class BEiT3(nn.Module):
+    def __init__(self, args, **kwargs):
+        super().__init__()
+        self.args = args
+        assert args.multiway
+        assert args.vocab_size > 0
+        assert not args.share_encoder_input_output_embed
+        self.text_embed = TextEmbedding(args.vocab_size, args.encoder_embed_dim)
+        self.vision_embed = VisionEmbedding(
+            args.img_size,
+            args.patch_size,
+            args.in_chans,
+            args.encoder_embed_dim,
+            contain_mask_token=True,
+            prepend_cls_token=True,
+        )
+        # being consistent with Fairseq, which starts from 2 for position embedding
+        embed_positions = MutliwayEmbedding(
+            modules=[
+                PositionalEmbedding(self.vision_embed.num_position_embeddings() + 2, args.encoder_embed_dim),
+                PositionalEmbedding(args.max_source_positions, args.encoder_embed_dim),
+            ],
+            dim=1,
+        )
+        self.encoder = Encoder(
+            args,
+            embed_tokens=None,
+            embed_positions=embed_positions,
+            output_projection=None,
+            is_encoder_decoder=False,
+        )
+
+    def forward(
+        self,
+        textual_tokens=None,
+        visual_tokens=None,
+        text_padding_position=None,
+        attn_mask=None,
+        vision_masked_position=None,
+        incremental_state=None,
+        positions=None,
+    ):
+        assert textual_tokens is not None or visual_tokens is not None
+
+        if textual_tokens is None:
+            x = self.vision_embed(visual_tokens, vision_masked_position)
+            encoder_padding_mask = None
+            multiway_split_position = -1
+        elif visual_tokens is None:
+            x = self.text_embed(textual_tokens)
+            encoder_padding_mask = text_padding_position
+            multiway_split_position = 0
+        else:
+            x1 = self.vision_embed(visual_tokens, vision_masked_position)
+            multiway_split_position = x1.size(1)
+            x2 = self.text_embed(textual_tokens)
+            x = torch.cat([x1, x2], dim=1)
+
+            if text_padding_position is not None:
+                encoder_padding_mask = torch.cat(
+                    [
+                        torch.zeros(x1.shape[:-1]).to(x1.device).bool(),
+                        text_padding_position,
+                    ],
+                    dim=1,
+                )
+            else:
+                encoder_padding_mask = None
+
+        encoder_out = self.encoder(
+            src_tokens=None,
+            encoder_padding_mask=encoder_padding_mask,
+            attn_mask=attn_mask,
+            token_embeddings=x,
+            multiway_split_position=multiway_split_position,
+            incremental_state=incremental_state,
+            positions=positions,
+        )
+        encoder_out["multiway_split_position"] = multiway_split_position
+
+        return encoder_out
         # Copyright (c) 2023 Microsoft
-# Licensed under The MIT License [see LICENSE for details]
+# Licensed under The MIT License [see LICENSE for details] (https://github.com/microsoft/torchscale/blob/main/torchscale/model/LongNet.py)
 
-import bisect
-import copy
-import json
-import re
-import string
-from collections import defaultdict
-from typing import List, Union
 
-import nltk
-import numpy as np
-import tiktoken
-import torch
-import torch.nn.functional as F
-from torch.utils.data import DataLoader
-from transformers import (
-    AutoConfig,
-    AutoModelForCausalLM,
-    AutoModelForTokenClassification,
-    AutoTokenizer,
-)
+class LongNetDecoderLayer(DecoderLayer):
 
-from .utils import (
-    TokenClfDataset,
-    get_pure_token,
-    is_begin_of_new_word,
-    process_structured_json_data,
-    remove_consecutive_commas,
-    replace_added_token,
+    def build_self_attention(self, embed_dim, args):
+        return DilatedAttention(
+            args,
+            embed_dim,
+            args.decoder_attention_heads,
+            dropout=args.attention_dropout,
+            self_attention=True,
+            encoder_decoder_attention=False,
+            subln=args.subln,
+        )
+
+class LongNetDecoder(Decoder):
+
+    def build_decoder_layer(
+        self, args, depth, is_moe_layer=False, is_encoder_decoder=False
+    ):
+        layer = LongNetDecoderLayer(
+            args,
+            depth,
+            is_moe_layer=is_moe_layer,
+            is_encoder_decoder=is_encoder_decoder,
+        )
+        if args.checkpoint_activations:
+            layer = checkpoint_wrapper(layer)
+        if args.fsdp:
+            layer = wrap(layer)
+        return layer
+
+class LongNetEncoderLayer(EncoderLayer):
+
+    def build_self_attention(self, embed_dim, args):
+        return DilatedAttention(
+            args,
+            embed_dim,
+            args.encoder_attention_heads,
+            dropout=args.attention_dropout,
+            self_attention=True,
+            encoder_decoder_attention=False,
+            subln=args.subln,
+        )
+
+class LongNetEncoder(Encoder):
+    def build_encoder_layer(
+        self, args, depth, is_moe_layer=False, is_encoder_decoder=False
+    ):
+        layer = LongNetEncoderLayer(
+            args,
+            depth,
+            is_moe_layer=is_moe_layer,
+            is_encoder_decoder=is_encoder_decoder,
+        )
+        if args.checkpoint_activations:
+            layer = checkpoint_wrapper(layer)
+        if args.fsdp:
+            layer = wrap(layer)
+        return layer
